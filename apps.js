10614d4ad75b3ded46e2086335a4ed2e9b1dc419
Besson Mikael <73292500+MikaelBesson@users.noreply.github.com>
2021-01-21 10:28:04 +0100

    memory game

---
+++ apps.js
@@ -0,0 +1,10 @@
+
+
+let cartes = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10];
+
+let plateau = document.getElementById("bordure");
+for (let i = 0; i < cartes>length; i++){
+    carteFace = cartes[i]
+
+
+}
